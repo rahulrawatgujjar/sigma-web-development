69a8ecdd57cded9dfc9cf267f58707669509d11b
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-30 17:46:38 +0530

    writeFile method of fs package
    It is used to write in a file asynchronously.
    It also provide functionality of callback to do task in sync.
    But it arise the problem of callback hell.

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const fs = require("fs");
 // console.log(fs);
 
+
+// // sync writing 
+// console.log("start");
+// fs.writeFileSync("rahul.txt","Rahul is a good boy.");
+// console.log("ending");
+
+
+// async writing 
 console.log("start");
-fs.writeFileSync("rahul.txt","Rahul is a good boy.");
+fs.writeFile("rahul2.txt","Rahul is a good boy.",()=>{
+  console.log("done");
+  fs.readFile("rahul2.txt",(error, data)=>{
+    console.log(error, data.toString());
+    // again
+    fs.appendFile("rahul2.txt","\n\nRahul is a good boy.",()=>{
+      console.log("done");
+      fs.readFile("rahul2.txt",(error, data)=>{
+        console.log(error, data.toString());
+      })
+    })
+  })
+})
 console.log("ending");
