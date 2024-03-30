e02a3cf08a4562398544e83611d061d2cce407a0
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-30 14:01:08 +0530

    video 86

--- server.js
+++
@@ -1,8 +0,0 @@
-var slugify = require('slugify')
-
-let a = slugify('some string') // some-string
-console.log(a)
-
-// if you prefer something other than '-' as separator
-let b = slugify('some string r&&*#ajgpaog', '_')  // some_string
-console.log(b)
