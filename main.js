54322b1e482221656e8e3d0de52a1a850ad42ee5
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-30 14:25:20 +0530

    default export
    here we use default keyword with export.
    We can import it as any name.

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-import {a, b} from "./mymodule.js"
-console.log(a,b)
+import {a, b} from "./mymodule.js";
+console.log(a,b);
+
+import harry from "./mymodule.js";
+
+console.log(harry);
--- mymodule.js
+++ mymodule.js
@@ -2,3 +2,12 @@
 
 export const a= "rahul";
 export const b= "rawat";
+
+// default export
+
+const obj={
+  x: "sahil",
+  y: "surender"
+}
+
+export default obj;
