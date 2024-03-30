a30ce5ed6f8cba8a2ef03f72fa7ee59180dfe9ae
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-30 14:43:37 +0530

    we can also use es6 in frontend using type="module" in script tag

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// import {a, b} from "./mymodule.js";
-// console.log(a,b);
-
-// import harry from "./mymodule.js";
-
-// console.log(harry);
-
-const {a,b}= require("./mymodule2.js");
+import {a, b} from "./mymodule.js";
 console.log(a,b);
+
+import harry from "./mymodule.js";
+
+console.log(harry);
+
+// const {a,b}= require("./mymodule2.js");
+// console.log(a,b);
