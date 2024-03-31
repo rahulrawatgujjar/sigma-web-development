77cc124528dec25e8b4dbd16f7e6e6a97949925a
Rahul Rawat <rahulrawatr320@gmail.com>
2024-03-31 14:42:22 +0530

    learning about express
    Routing using express.
    Methods for routing like get, post, put, delete etc.
    Route parameters concept

---
+++ main.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get or app.post app.put or app.delelte(path, handler)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/about", (req, res)=>{
+  res.send("This is about page.")
+})
+
+app.get("/contact", (req, res)=>{
+  res.send("This is contact page.")
+})
+
+app.get("/blog/:slug/:hello", (req, res)=>{
+  res.send(`This is ${req.params.slug} - ${req.params.hello}`);
+  // routing parameters
+  console.log(req.params);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
