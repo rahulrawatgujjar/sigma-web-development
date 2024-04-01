25f4cc36180e39982f50f2394d29afe0fc4aded6
Rahul Rawat <rahulrawatr320@gmail.com>
2024-04-01 17:08:02 +0530

    middleware specific to a Router

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+
+// this middleware is specific to this router (ie It work for all routes in this blog.js file)
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+
 router.get("/",(req, res)=>{
   res.send("Blog HomePage");
 });
