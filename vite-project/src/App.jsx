f223cb6819af84742509a4f34ff149db264226ce
Rahul Rawat <rahulrawatr320@gmail.com>
2024-04-26 16:20:28 +0530

    useMemo hook in react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,55 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import { counterContext } from './context/context'
+
+const nums = new Array(30_000_000).fill(0).map((_, i) => {
+  return {
+    index: i,
+    isMagical: i === 29_000_000
+  }
+})
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [numbers, setNumbers] = useState(nums);
+
+  // const magical = numbers.find(item=>item.isMagical)
+  const magical = useMemo(() => numbers.find(item => item.isMagical), [numbers])
 
   return (
     <>
-      <counterContext.Provider value={{count,setCount}}>
-        <Navbar/>
-        <button className='bg-green-400 m-3 p-1 border border-black rounded-md' onClick={() => setCount((count) => count + 1)}>
+      <div>
+        <div>Magical number is {magical.index}</div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => {
+          setCount((count) => count + 1);
+          if (count===10){
+            setNumbers(new Array(30_000_000).fill(0).map((_, i) => {
+              return {
+                index: i,
+                isMagical: i === 1_000_000
+              }
+            }))
+          }
+        }}>
           count is {count}
         </button>
-      </counterContext.Provider>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
