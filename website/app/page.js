6aa8dabd7f5ac89648e9df1bf6e2879d87d41b53
Rahul Rawat <rahulrawatr320@gmail.com>
2024-04-29 12:34:40 +0530

    More about Image component
    if we use fill= ture then no need of width and height to be set
    just make its parent be relative
    And set property like object-cover contain fill etc

--- website/app/page.js
+++ website/app/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const home = () => {
   return (
-    <div className="container mx-auto bg-pink-400 my-3 size-80">
-      <Image width={300} height={300} className='mx-auto' src="https://www.simplilearn.com/ice9/free_resources_article_thumb/html_image_slider.jpg" alt="" />
+    <div className="container mx-auto relative bg-pink-400 my-3 size-80">
+      <Image fill={true} className='mx-auto object-cover' src="https://www.simplilearn.com/ice9/free_resources_article_thumb/html_image_slider.jpg" alt="" />
     </div>
   )
 }
