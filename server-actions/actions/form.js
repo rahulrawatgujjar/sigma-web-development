0d221d8ac0a0e25762b395720b88091118a803d2
Rahul Rawat <rahulrawatr320@gmail.com>
2024-04-30 17:43:00 +0530

    middleware in next.js
    In this case each url that matches with matcher in config will be redirected to /about

--- server-actions/actions/form.js
+++
@@ -1,8 +0,0 @@
-"use server"
-import fs from "fs/promises";
-
-export const submitAction = async (e) => {
-  console.log(e);
-  console.log(`Name: ${e.get("name")}\nAddress: ${e.get("address")}`);
-  let a = await fs.writeFile("rahul.txt", `Name: ${e.get("name")}\n Address: ${e.get("address")}`);
-}
