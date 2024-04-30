6d2d259b6efddbf08c2f7bf8fab8fda4fdcccd59
Rahul Rawat <rahulrawatr320@gmail.com>
2024-04-30 17:46:24 +0530

    middleware with conditional statements

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,11 +1,24 @@
+// import { NextResponse } from 'next/server'
+ 
+// // This function can be marked `async` if using `await` inside
+// export function middleware(request) {
+//   return NextResponse.redirect(new URL('/about', request.url))
+// }
+ 
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: '/about2/:path*',
+// }
+
+
 import { NextResponse } from 'next/server'
  
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/about', request.url))
-}
+  if (request.nextUrl.pathname.startsWith('/about-2')) {
+    return NextResponse.rewrite(new URL('/about', request.url))
+  }
  
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/about2/:path*',
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.rewrite(new URL('/', request.url))
+  }
 }
